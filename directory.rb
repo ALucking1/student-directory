40923164a0de75e1ac2409be1e5865c755b0374a
Anna Lucking <annavlucking@hotmail.co.uk>
2016-09-17 21:13:27 +0100

    Updated ex.9

--- directory.rb
+++ directory.rb
@@ -12,10 +12,6 @@ def input_students
       puts "Please enter student cohort month (January, February, etc.)"
       cohort = gets.chomp
       if cohort.empty?
-        puts "Please enter student cohort month (January, February, etc.)"
-        cohort = gets.chomp
-      end
-      if cohort.empty?
         cohort = "January"
       end
     cohort.to_sym
@@ -27,27 +23,36 @@ def input_students
     answer = gets.chomp
       if answer.include?("n")
         input == true
+      else students << {name: name, cohort: cohort, age: age}
+        #puts student_number(students)
+      end
+
+
+      if students.count > 1
+        puts "Now we have #{students.count} students"
+      elsif students.count == 0
+        puts "We have no students"
       else
-        students << {name: name, cohort: cohort, age: age}
-        puts student_number(students)
+        puts "Now we have #{students.count} student"
+      end
+
         puts "Any more students to add? (yes or no)"
         input = gets.downcase.include?("y") ? true : false
-      end
-    end
+  end
   #return the array of students
   students
 end
 
-def student_number(students)
-  n = students.count
-    if n > 1
-      puts "Now we have #{students.count} students"
-    elsif n == 0
-      puts "We have no students"
-    else
-      puts "Now we have #{students.count} student"
-    end
-end
+#def student_number(students)
+  #n = students.count
+    #if n > 1
+      #puts "Now we have #{students.count} students"
+    #elsif n == 0
+      #puts "We have no students"
+    #else
+      #puts "Now we have #{students.count} student"
+    #end
+#end
 
 def print_header
   puts "The students of Villains Academy".center(50)
@@ -55,13 +60,19 @@ def print_header
 end
 
 def print(students)
-      students.sort_by do |k, v|
-      puts "#{k[:cohort]} cohort, #{k[:name]}, #{k[:age]}".center(50)
-  end
+    students.sort_by do |k, v|
+    puts "#{k[:cohort]} cohort, #{k[:name]}, #{k[:age]}".center(50)
+    end
 end
 
 def print_footer(students)
-  puts "Overall, we have #{students.count} great students".center(50)
+  if students.count > 1
+    puts "Overall, we have #{students.count} students".center(50)
+  elsif students.count == 0
+    puts "Overall, we have no students".center(50)
+  else
+    puts "Overall, we have #{students.count} student".center(50)
+  end
 end
 
 students = input_students
