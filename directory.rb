5dc82e742b0a6d5035a87108c8ae5852fddf8b56
Anna Lucking <annavlucking@hotmail.co.uk>
2016-09-19 09:56:15 +0100

    12.2 - default students.csv file

--- directory.rb
+++ directory.rb
@@ -1,5 +1,37 @@
 @students = []
 
+def print_menu
+  puts "1. Input the students"
+  puts "2. Show the students"
+  puts "3. Save the list to #{@filename}"
+  puts "4. Load the list from #{@filename}"
+  puts "9. Exit"
+end
+
+def interactive_menu
+  loop do
+    print_menu
+    process(STDIN.gets.chomp)
+  end
+end
+
+def process(selection)
+  case selection
+    when "1"
+      input_students
+    when "2"
+      show_students
+    when "3"
+      save_students
+    when "4"
+      load_students
+    when "9"
+      exit
+    else
+      puts "I don't know what you mean, try again"
+  end
+end
+
 def input_students
   input = true
 
@@ -23,7 +55,7 @@ def input_students
     answer = STDIN.gets.chomp
       if answer.include?("n")
         input == true
-      else #@students << {name: name, cohort: cohort.to_sym, age: age}
+      else
         students_to_array
       end
 
@@ -40,19 +72,8 @@ def input_students
     end
 end
 
-def interactive_menu
-  loop do
-    print_menu
-    process(STDIN.gets.chomp)
-  end
-end
-
-def print_menu
-  puts "1. Input the students"
-  puts "2. Show the students"
-  puts "3. Save the list to students.csv"
-  puts "4. Load the list from students.csv"
-  puts "9. Exit"
+def students_to_array
+  @students << {name: @name, cohort: @cohort.to_sym, age: @age}
 end
 
 def show_students
@@ -61,31 +82,12 @@ def show_students
   print_footer
 end
 
-def process(selection)
-  case selection
-    when "1"
-      input_students
-    when "2"
-      show_students
-    when "3"
-      save_students
-    when "4"
-      load_students
-    when "9"
-      exit
-    else
-      puts "I don't know what you meant, try again"
-  end
-end
-
-def students_to_array
-  @students << {name: @name, cohort: @cohort.to_sym, age: @age}
-end
-
 def print_header
   puts "The students of Villains Academy".center(50)
   puts "-------------".center(50)
 end
+
+
 
 def print_students_list
   @students.sort_by do |k, v|
@@ -105,7 +107,7 @@ end
 
 def save_students
   #open the file for writing
-  file = File.open("students.csv", "w")
+  file = File.open("#{@filename}", "w")
   #iterate over the array of students
   @students.each do |student|
     student_data = [student[:name], student[:cohort], student[:age]]
@@ -115,26 +117,28 @@ def save_students
   file.close
 end
 
-def load_students(filename = "students.csv")
-  file = File.open("students.csv", "r")
+def load_students
+  file = File.open(@filename, "r")
   file.readlines.each do |line|
   name, cohort, age = line.chomp.split(',')
     students_to_array
-    #@students << {name: name, cohort: cohort.to_sym, age: age}
   end
   file.close
 end
 
 def try_load_students
-  filename = ARGV.first #first argument from the command line
-  return if filename.nil? #get out of the method if it isn't given
-  if File.exists?(filename)
-    load_students(filename)
-      puts "Loaded #{@students.count} from #{filename}"
-  else #if it doesn't exists
-    puts "Sorry, #{filename} doesn't exist"
+  @filename = ARGV.first #first argument from the command line
+  if @filename.nil?
+    @filename = "students.csv"
+    puts "Loaded #{@students.count} from #{@filename}"
+  elsif File.exists?(@filename)
+    load_students
+      puts "Loaded #{@students.count} from #{@filename}"
+  else
+    puts "Sorry, #{@filename} doesn't exist"
     exit
   end
 end
 
+try_load_students
 interactive_menu
